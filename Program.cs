c674cc1547b53846d300041a39b301b53af89af5
Prakashkp <k.p.prakash86@gmail.com>
2023-08-25 15:51:25 +0530

    Update Program.cs
    Added comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using SummationTask.Classes;
+// This class is the entry point on the application
+using SummationTask.Classes;
 using SummationTask.Interface;
 
 namespace SummationTask
@@ -9,13 +10,13 @@ namespace SummationTask
         {
             Console.WriteLine("Enter n to find sum of first n natural numbers");
             var n = Convert.ToDouble(Console.ReadLine());
-            //injecting the dependent class and creating facade class for sequence of task and client wont know about the complex of the process
+            //injecting the dependent class based on SOLID principle and creating facade class for sequence of task and client wont know about the complex of the process
             var summationSeriesFacade = new NaturalSummationFacade(SummationFactory.GetNaturalSeriesSummation());
             // Processing the facade class
             var resultNaturalSummation = summationSeriesFacade.Processs(n);
             //Printing the result
             Console.WriteLine("∑i^n = 1 + 2 + 3 + ... + n: " + resultNaturalSummation.SummationResult.ToString());
-            //injecting the dependent class and creating facade class for sequence of task and client wont know about the complex of the process
+            //injecting the dependent class on SOLID principle and creating facade class for sequence of task and client wont know about the complex of the process
             var summationSquareSeriesFacade = new NaturalSquareSummationFacade(SummationFactory.GetNaturalSeriesSquareSummation());
             // Processing the facade class
             var resultNaturalSquareSummation = summationSquareSeriesFacade.Processs(n);
